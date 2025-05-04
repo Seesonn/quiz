6c3138d995d3d3ad1c012c25b865d2cc17836e4e
Seesonn <sisanbhattarai001@gmail.com>
2025-05-04 21:33:06 +0545

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ export default function Home() {
       speechSequence.push(
         { text: `Incorrect. correct answer ${correctAnswer}.`, rate: 1.0 },
         {
-          text: "Don't be sad! Even Google can't help you now — better dial Sisan Bhattarai as soon as possible.",
+          text: "Don't be sad! better dial Sisan Bhattarai as soon as possible.",
           rate: 0.9,
         }
       );
